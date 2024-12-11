8cf8dcc1cee3307c74cdf0cdf08a5e9e60af968a
sriramthirukumar <sriramthirukumar@gmail.com>
2024-12-11 22:40:57 +0530

    HelloWorld Program added

--- src/main/scala/lectures/part1basics/HelloWorld.scala
+++ src/main/scala/lectures/part1basics/HelloWorld.scala
@@ -3,5 +3,12 @@ package lectures.part1basics
 object HelloWorld {
   def main(args: Array[String]) : Unit = {
     println("Hello World")
+    var result = add(10, 20)
+//    println("Addition of given numbers is: "+result)
+    println(s"Addition of given numbers is: $result")
+  }
+  def add(x: Int, y:Int): Int = {
+    var z = x + y
+    return z
   }
 }
