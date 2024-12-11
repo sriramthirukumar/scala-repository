0ca79224bebf3e0e029c999190d773d96d74aaba
sriramthirukumar <sriramthirukumar@gmail.com>
2024-12-12 00:56:51 +0530

    demo for class and object

--- src/main/scala/lectures/part1basics/demoClassObject.scala
+++ src/main/scala/lectures/part1basics/demoClassObject.scala
@@ -1,5 +1,30 @@
 package lectures.part1basics
 
-object demoClassObject {
-println("Hello")
+class Car {
+  var extraCost: Int = 0
+  private var roadTax: Int = 100
+
+  def cost(basicCost: Int): Int ={
+    basicCost + roadTax + extraCost
+  }
+  def printTax()={
+    roadTax = roadTax+10
+    roadTax
+  }
+  def confirmTax() = {
+    roadTax
+  }
+
+}
+object demoClassObject extends App {
+  println("Hello")
+  var bmw = new Car
+  bmw.extraCost = 100
+  var roadTaxInObject = bmw.printTax()
+  println("The Tax of bmw is: "+roadTaxInObject)
+//  in above call updated tax value from class is called so every
+//   other calls will be having updated value only
+  println("Confirmed Tax is: "+bmw.confirmTax())
+  var result = bmw.cost(10000)
+  println("Total cost of bmw is: "+result)
 }
