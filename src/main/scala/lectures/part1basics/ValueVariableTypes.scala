2488538f04e12307409d57a5f847a91c0359c934
sriramthirukumar <sriramthirukumar@gmail.com>
2024-12-11 21:52:41 +0530

    HelloWorld Program added

--- src/main/scala/lectures/part1basics/ValueVariableTypes.scala
+++ src/main/scala/lectures/part1basics/ValueVariableTypes.scala
@@ -1,5 +1,13 @@
 package lectures.part1basics
 
-object ValueVariableTypes {
-
+object ValueVariableTypes extends App {
+  val x: Int = 27
+  val aString: String ="""Sriram is a
+                        |very good programmer""".stripMargin
+  val aBoolean: Boolean = true
+  val aShort: Short = 12345
+  val aLong: Long = 12345678901L
+  val aFloat: Float = 2.0F
+  val aDouble: Double = 3.14
+  println(aString)
 }
